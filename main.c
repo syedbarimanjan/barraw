6d8e692be567db09a260d1aa9c403d5cc1ea63bf
syedbarimanjan <syedbarimanjan@gmail.com>
2026-03-16 12:14:43 +0500

    add basic rectangle and circle

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ typedef enum Tools {
   PEN,
   LINE,
   RECTANGLE,
+  CIRCLE,
 } Tools;
 
 Tools tools;
@@ -28,7 +29,7 @@ int main() {
 
   while(!WindowShouldClose()) {
 
-    tools = LINE;
+    tools = RECTANGLE;
     if(IsMouseButtonDown(MOUSE_BUTTON_LEFT) && tools == PEN && mouseWasPressed == false ){
       // mouseWasPressed = true;
       int x= GetMouseX();
@@ -36,6 +37,9 @@ int main() {
 
       BeginTextureMode(target);
       DrawCircle(x,y,10,RED);
+
+      DrawRectangle(10,10,100,100,BLACK);
+      DrawFPS(10,10);
             
       EndTextureMode();
       
@@ -54,6 +58,48 @@ int main() {
       int y= GetMouseY();
       BeginTextureMode(target);
       DrawLine(xline,yline,x,y,GREEN);
+      
+      DrawRectangle(10,10,100,100,BLACK);
+      DrawFPS(10,10);
+      EndTextureMode();
+      
+    }
+    int xlinerec;
+    int ylinerec;
+    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && tools == RECTANGLE && mouseWasPressed == false) {
+      mouseWasPressed = true;
+      xlinerec= GetMouseX();
+      ylinerec= GetMouseY();
+    } else if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && tools == RECTANGLE && mouseWasPressed) {
+      mouseWasPressed = false;
+      int x= GetMouseX();
+      int y= GetMouseY();
+      BeginTextureMode(target);
+      DrawRectangleLines(xlinerec,ylinerec,x-xlinerec,y-ylinerec,BLUE);
+      DrawCircle((xlinerec+x)/2,(ylinerec+y)/2,10,RED);
+      
+      DrawRectangle(10,10,100,100,BLACK);
+      DrawFPS(10,10);
+      EndTextureMode();
+      
+    }
+    int xlinecircle;
+    int ylinecircle;
+    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && tools == CIRCLE && mouseWasPressed == false) {
+      mouseWasPressed = true;
+      xlinecircle= GetMouseX();
+      ylinecircle= GetMouseY();
+    } else if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && tools == CIRCLE && mouseWasPressed) {
+      mouseWasPressed = false;
+      int x= GetMouseX();
+      int y= GetMouseY();
+      BeginTextureMode(target);
+
+      float dx = x-xlinecircle;
+      float dy = y -ylinecircle;
+      float radius = sqrt(dx*dx + dy*dy);
+      
+      DrawCircleLines((xlinecircle+x)/2,(ylinecircle+y)/2,radius/2,PINK);
       
       DrawRectangle(10,10,100,100,BLACK);
       DrawFPS(10,10);
